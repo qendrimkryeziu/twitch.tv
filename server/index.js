fb7f12985d16283b2ffa2724357f3ce283a0dd3b
Qendrim <qendrimkryeziu18@gmail.com>
2023-11-26 14:22:58 -0800

    Creating Express / Authentication Routes / MongoDB

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoutes from "./src/routes/authRoutes.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.send("Hello here is your server");
+});
+
+app.use("/api/auth", authRoutes);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed. Server not started");
+    console.log(err);
+  });
