fc35e4a4613f07d6dbed64c94a8cabb3f50b2489
Qendrim <qendrimkryeziu18@gmail.com>
2023-11-29 14:49:03 -0800

    react app / with auth

---
+++ client/src/AuthPage/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Logo } from "./Logo";
+
+export const Login = ({ switchAuthHandler }) => {
+  return (
+    <div className="login-container">
+      <Logo text={"Log in to Clone"} />
+      <form className="auth-form">Form</form>
+      <span onClick={switchAuthHandler} className="auth-form-switch-label">
+        Don't have an account ? Sign up
+      </span>
+    </div>
+  );
+};
